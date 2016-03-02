d14cd5a7c5d369b515f779f7f62e7073b2fa0aec
tonyyanxuan <tony0620emma@yahoo.com.tw>
2016-03-02 13:58:27 +0800

    Show pid for the module

--- hello/hello.c
+++ hello/hello.c
@@ -1,10 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 MODULE_LICENSE("GPLv3");
 
 static int hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world!!\n");
+	printk(KERN_INFO "The process is \"%s\" (pid %i)\n", current->comm, current->pid);
 	return 0;
 }
 
